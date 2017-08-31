86efe62a746668ad33c72148b64559c2e231de3b
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2017-08-31 23:04:54 +0530

    Update Num.c

--- Num.c
+++ Num.c
@@ -1,14 +1,18 @@
 #include<stdio.h> 
+#include<conio.h>
 void main()
 {
 int a,b=0;
+clrscr(); 
   Scanf(%d,&a);
 if(a>=b)
 {
   printf("num is positive");
 }
 else
+  {
 Prinf("num is negative");
+  }
 }
 
 
